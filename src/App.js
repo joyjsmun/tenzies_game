fd437a4ac4f92f1e0cb4de990c38598de8f4304f
joyjsmun <joy.js.mun@gmail.com>
2022-03-31 21:34:41 +0900

    generate random num

--- src/App.js
+++ src/App.js
@@ -2,10 +2,31 @@ import Die from "./components/Die"
 
 
 function App() {
+  
+function allNewDice() {
+  const newDice = [];
+  for(let i=0; i<10; i++){
+    newDice.push(Math.ceil(Math.random()*6))
+  }  
+  return newDice
+}
+
+console.log(allNewDice())
+
   return (
    <main>
      <div className="container">
-        <Die value={1}/>
+       
+     </div>
+     <button onClick={allNewDice}>dice</button>
+   </main>
+  );
+}
+
+export default App;
+
+
+ /* <Die value={1}/>
         <Die value={2}/>
         <Die value={3}/>
         <Die value={4}/>
@@ -14,10 +35,4 @@ function App() {
         <Die value={6}/>
         <Die value={1}/>
         <Die value={1}/>
-        <Die value={2}/>
-     </div>
-   </main>
-  );
-}
-
-export default App;
+        <Die value={2}/> */
