9016cfd12bf126889fbe8c45e10cc2b711331316
joyjsmun <joy.js.mun@gmail.com>
2022-04-01 19:57:44 +0900

    added react-confetti

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Die from "./components/Die"
 import {nanoid} from "nanoid";
+import Confetti from 'react-confetti'
 
 
 function App() {
@@ -54,15 +55,17 @@ function rollDice() {
   }))
 }
 
+
   return (
    <main>
+     {tenzies ? <Confetti width={window.innerWidth} height={window.innerHeight} /> : null}
       <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container"></div>
      <div className="container">
        {diceElement}
      </div>
-     <button onClick={rollDice}>Roll</button>
+     <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
    </main>
   );
 }
