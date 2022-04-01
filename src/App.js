16af36fc226b56659d0972a10640025f4f6edaaf
joyjsmun <joy.js.mun@gmail.com>
2022-04-01 19:42:14 +0900

    added useEffect for checking the same values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Die from "./components/Die"
 import {nanoid} from "nanoid";
 
 
 function App() {
   const [newDice,setNewDice] = useState(allNewDice())
+  const [tenzies,setTenzies] = useState(false)
+
+useEffect(() => {
+  const allHeld = newDice.every(die => die.isHeld === true)
+  const firstValue = newDice[0].value
+  const allSameVale = newDice.every(die => die.value === firstValue )
+  if(allHeld && allSameVale) {
+    console.log("You Won")
+    setTenzies(true)
+  }
+
+
+},[newDice])
 
 function generateNewDice() {
   return {
@@ -43,6 +56,9 @@ function rollDice() {
 
   return (
    <main>
+      <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container"></div>
      <div className="container">
        {diceElement}
      </div>
